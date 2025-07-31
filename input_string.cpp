d2c9b551c29892b986fb530b7c6e24b6d6561b34
AntarleenChand05 <antarleen.chand.btech2024@sitpune.edu.in>
2025-07-31 09:22:48 +0530

    Create input_string.cpp

---
+++ input_string.cpp
@@ -0,0 +1,23 @@
+// Antarleen Chand
+// 24070123018 | ENTC A1 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string userInput;
+
+    cout << "Enter a string: ";
+    getline(cin, userInput); 
+
+    cout << "You entered: " << userInput << endl;
+
+    return 0;
+}
+
+/*
+OUTPUT:
+Enter a string: champagne papi
+You entered: champagne papi
+*/
