2a78ae623b7d559ac38a1c7ebbe0379b4842cb27
AntarleenChand05 <antarleen.chand.btech2024@sitpune.edu.in>
2025-07-31 09:52:55 +0530

    Create check_palindrome.cpp

---
+++ check_palindrome.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    cout << "You entered: " << str << endl;
+
+    string reversed_str = "";
+    for (int i = str.length() - 1; i >= 0; i--) {
+        reversed_str += str[i];
+    }
+
+    if (str == reversed_str) {
+        cout << "The string is a palindrome." << endl;
+    } else {
+        cout << "The string is not a palindrome." << endl;
+    }
+
+    return 0;
+}
+/*OUTPUT : 
+Enter a string: ROTATOR LEVEL
+You entered: ROTATOR LEVEL
+The string is not a palindrome.
+*/
